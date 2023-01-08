cac25cfdadd0c972258c203b4f02261c3b2da855
nabil <nabil.nader00@gmail.com>
2023-01-08 23:35:09 +0200

    add authorities

--- src/main/java/com/example/ssf/config/WebSecurityConfig.java
+++ src/main/java/com/example/ssf/config/WebSecurityConfig.java
@@ -8,18 +8,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class WebSecurityConfig {
 
-    /*
-        here we are using the default configuration,which is implemented by spring boot
-        webSecurityConfigureAdapter is npt used anymore , we will do it in a new way => it is still a very easy way
-        using security filter chain bean which will allow to set a different authentication provider not a manager
-        tha manger => is always the same
-        authentication provider -> is what you will implement your self -> this will be later
-
-     */
-
-    /*
-        this is a default lesson 4 will be custom authentication
-     */
+ /*
+    our customer here do not have users, our custom just relies on key
+  */
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/com/example/ssf/reposiotrty/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ssf.reposiotrty;
-
-import com.example.ssf.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority,Long> {
-}
