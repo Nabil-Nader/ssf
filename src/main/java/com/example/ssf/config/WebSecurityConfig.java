5c23bca364fb5a1f6ce696307a9fef75bd356e74
nabil.nader <nabil.nader00@gmail.com>
2023-09-10 17:29:29 +0300

    working with lecture 5

--- src/main/java/com/example/ssf/config/WebSecurityConfig.java
+++ src/main/java/com/example/ssf/config/WebSecurityConfig.java
@@ -2,10 +2,17 @@ package com.example.ssf.config;
 
 import com.example.ssf.security.filter.ApiKeyFilter;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -36,29 +43,34 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        /*
 
-        http basic is an http filter and our own filter is ApiKeyFilter
-         http filter will delegate to its own authentication manger and if we want to complete our own manger
-         we will create it
-         */
-        /*
-            Let's look at wht is HttpSecurity,
-            is an object was previously used by extending WebSecurityConfigureAdapter.Class
-            and we override the configure method
-            spring security team changed the behavior, they decouple us using an extending a class
-            it is better to use has a relationship rather than extending class
-            Now we but a Bean in the context, and now this HTTP security parameter it has been pushed into spring context
-            being in spring context you can anytime just get it through the parameter of any bean method
-            this object create behind the scenes the entire structure
-         */
         return http.httpBasic()
                 .and()
-                .addFilterBefore(new ApiKeyFilter(key), BasicAuthenticationFilter.class)
-                .authorizeRequests().anyRequest().authenticated() // without this line by API can send a request and receive a 200
-                //one more thing this authorization will discuss in lesson 5
+                .authorizeRequests()
+                .anyRequest().authenticated() // without this line by API can send a request and receive a 200
                 .and()
                 .build();
+//        Matcher method + authorization rule
+//        1- Which matcher you should use and how
+//        2- how to apply different authorization rule
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService (){
+        var uds = new InMemoryUserDetailsManager();
+
+        var user  = User.withUsername("billy")
+                .password(passwordEncoder().encode("pass"))
+                .authorities("read")
+                .build();
+
+        uds.createUser(user);
+        return uds;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 
