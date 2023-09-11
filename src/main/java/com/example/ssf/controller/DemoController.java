72708f407a620085696fb9dbce4f39fc2b418f0b
nabil.nader <nabil.nader00@gmail.com>
2023-09-11 18:33:00 +0300

    done with l6/endpoint_part2

--- src/main/java/com/example/ssf/controller/DemoController.java
+++ src/main/java/com/example/ssf/controller/DemoController.java
@@ -1,14 +1,21 @@
 package com.example.ssf.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/demo")
 public class DemoController {
 
-    @GetMapping("/demo")
-    public String demo(){
+    @GetMapping("/test1")
+    public String test1(){
 
-        return "demo";
+        return "demo 1";
+    }
+    @GetMapping("/test2")
+    public String test2(){
+
+        return "demo 2";
     }
 }
